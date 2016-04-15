193721661e910b7ef3dca837473d3e4a081517da
Xaratustrah <shahab.sanjari@gmail.com>
2016-04-15 16:18:27 +0200

    bump

--- setup_win.py
+++ setup_win.py
@@ -12,27 +12,34 @@ from version import __version__
 from glob import glob
 import PyQt5
 import py2exe
+import zmq
+import zmq.libzmq
 
 NAME = 'rdgui'
 
 packages = ['zmq']
 
-includes = ['sip', 'atexit', 'zmq', ]
+includes = ['sip', 'atexit', 'zmq', 'zmq.backend.cython']
 
-excludes = None
+excludes = ['zmq.libzmq']
+dll_excludes = ['libzmq.pyd']
 
 options = {
     'optimize': 2,
     'compressed': True,
     'includes': includes,
     'excludes': excludes,
+    'dll_excludes' : dll_excludes,
     'packages': packages
 }
 
 data_files = []
 
 qt_platform_plugins = [('platforms', glob(PyQt5.__path__[0] + r'\plugins\platforms\*.*'))]
+zmq_plugin = [('', glob(zmq.__path__[0] + r'\backend\cython\error.pyd'))]
+
 data_files.extend(qt_platform_plugins)
+data_files.extend(zmq_plugin)
 
 setup(
     name=NAME,
