0fd118df2902fa1b40b0e2c3ed806a234cf77d8e
xaratustrah <xaratustrah@users.noreply.github.com>
2024-02-14 17:14:44 +0100

    .

--- rdcli/__main__.py
+++ rdcli/__main__.py
@@ -63,13 +63,14 @@ def gpio_setup():
 
 def read_adc_channel(spi, channel):
     (msg_up, msg_dn) = (
-        (0x06, 0x00)
-        if channel == 0
-        else (0x06, 0x40)
-        if channel == 1
-        else (0x06, 0x80)
-        if channel == 2
-        else (0x06, 0xC0)
+        (0x06, 0x00) if channel == 0
+        else (0x06, 0x40) if channel == 1
+        else (0x06, 0x80) if channel == 2
+        else (0x06, 0xC0) if channel == 3
+        else (0x07, 0x00) if channel == 4
+        else (0x07, 0x40) if channel == 5
+        else (0x07, 0x80) if channel == 6
+        else (0x07, 0xC0) if channel == 7
     )
 
     resp = spi.xfer([msg_up, msg_dn, 0x00])
